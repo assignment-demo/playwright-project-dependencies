56a56501027c9e4fccf389a30c284fafcf2898b2
Chetan Panchal <cpemail.readonly@gmail.com>
2024-03-03 21:56:52 +0530

    updated

--- tests/e2e-loggedin.spec.ts
+++ tests/e2e-loggedin.spec.ts
@@ -1,17 +1,19 @@
 import { test, expect } from '@playwright/test';
 
+test.use({ launchOptions: { slowMo: 500 }})
+
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
 
 test('menu', async ({ page }) => {
-  await page.getByRole('link', { name: 'TestingLogin' }).click();
-  await expect(page.getByRole('heading', { name: /TestingLogin/i })).toBeVisible();
-  await page.getByRole('link', { name: /alerts/i  }).click();
-  await page.getByText('Alerts', { exact: true }).click();
-  await page.getByRole('button', { name: /notice/i  }).click();
-  await page.getByText('Notices').click();
-  await page.getByRole('link', { name: /watchlist/i  }).click();
+  // await page.getByRole('link', { name: process.env.UN }).click();
+  // await expect(page.getByRole('heading', { name: process.env.UN })).toBeVisible();
+  // await page.getByRole('link', { name: /alerts/i  }).click();
+  // await page.getByText('Alerts', { exact: true }).click();
+  // await page.getByRole('button', { name: /notice/i  }).click();
+  // await page.getByText('Notices').click();
+  // await page.getByRole('link', { name: /watchlist/i  }).click();
 
 })
 
